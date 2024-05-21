03f3eeba194c9ed369805a67f50090c9194f33ac
arBishal <m.arbishal@gmail.com>
2024-05-22 02:39:06 +0600

    Fix dashboard routes

--- src/components/admin/AdminDashboard.jsx
+++ src/components/admin/AdminDashboard.jsx
@@ -12,11 +12,21 @@ export default function AdminDashboard () {
       [history]
     );
 
+    const handleClickView = useCallback(
+        () => history.push("/admin/dashboard/view"),
+        [history]
+      );
+
+    const handleClickSchedule = useCallback(
+        () => history.push("/admin/dashboard/schedule"),
+        [history]
+      );
+
     return (
         <div className={adminDashboardStyle.dashboard}>
             <span className={adminDashboardStyle.button} onClick={handleClickAdd}> Add a Waterbus </span>
-            <span className={adminDashboardStyle.button}> View Waterbuses </span>
-            <span className={adminDashboardStyle.button}> Scedule a Waterbus </span>
+            <span className={adminDashboardStyle.button} onClick={handleClickView}> View Waterbuses </span>
+            <span className={adminDashboardStyle.button} onClick={handleClickSchedule}> Scedule a Waterbus </span>
             <span className={adminDashboardStyle.button}> Cancel a Trip </span>
             <span className={adminDashboardStyle.button}> Billing Statements </span>
 
