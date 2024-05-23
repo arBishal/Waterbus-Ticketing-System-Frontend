6a036793166528d843aa2fe4a8130e20e8bce0f2
safayetneyam <onyttric@gmail.com>
2024-05-24 00:04:49 +0600

    Add marquee

--- src/components/Billboard.jsx
+++ src/components/Billboard.jsx
@@ -3,7 +3,7 @@ import billboardStyle from "./billboard.module.css"
 export default function Billboard () {
     return (
         <div className={billboardStyle.billboard}>
-            ...this is a running Billboard...
+                <marquee>This is a basic scrolling text.</marquee>
         </div>
     );
 };
